d5ab94cf195f484bd1e7814a2907adca60ee3a81
archus18 <archanasonawane4534@gmail.com>
2026-02-03 16:06:55 +0530

    updatedbackend

--- client/Ecomm-App/src/api/apis.js
+++ client/Ecomm-App/src/api/apis.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_BACKEND_URL;
+const BASE = import.meta.env.VITE_BACKEND_URL +"/api";
 console.log(BASE);
 
 const authHeaders = () => {
