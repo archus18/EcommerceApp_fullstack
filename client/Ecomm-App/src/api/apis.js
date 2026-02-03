e547431a1c9ddd920f0ad327aef34012b5d7f989
archus18 <archanasonawane4534@gmail.com>
2026-02-03 12:25:28 +0530

    backendurlupdated

--- client/Ecomm-App/src/api/apis.js
+++ client/Ecomm-App/src/api/apis.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE = "http://localhost:3000/api";
+const BASE = "import.meta.env.VITE_BACKEND_URL";
+console.log(BASE);
 
 const authHeaders = () => {
   const token = localStorage.getItem("token");
